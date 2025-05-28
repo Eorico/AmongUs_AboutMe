def2c2f84311d6341508455243855cfcfd749630
Eorico Gonzales <eoricogonzales@gmail.com>
2025-05-29 07:55:21 +0800

    Update

--- js1/firebase.js
+++ js1/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB966LQtDSGb2LnZ_jMHFMYsHfwjRviU18",
   authDomain: "websiteaddimage.firebaseapp.com",
   projectId: "websiteaddimage",
-  storageBucket: "websiteaddimage.firebasestorage.app",
+  storageBucket: "websiteaddimage.appspot.com",
   messagingSenderId: "644122614522",
   appId: "1:644122614522:web:5c7f41285c1e1f58784743",
   measurementId: "G-LD41MRN5Z7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
